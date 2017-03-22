3fe643b9fac08b5141e7311b4c909e8a363e0431
Alberto Montes <alm59321@gmail.com>
2017-03-22 20:00:02 +0100

    Add missing line to header file

--- implementations/original/io.h
+++ implementations/original/io.h
@@ -1,3 +1,4 @@
 
 bool load_data(double* data, int n, int* d, char* data_file);
 void save_data(double* data, double* costs, int n, int d, char* data_file);
+void save_numeric_data(double* data, int N, char* data_file);
