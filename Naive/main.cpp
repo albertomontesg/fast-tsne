9f6fdeff7d8fded7cf165c8df6168549cef777ac
Marc Fischer <spam@totallyawesome.at>
2017-04-29 19:12:27 +0200

    updated naive impelmentation

--- Naive/main.cpp
+++ Naive/main.cpp
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 const size_t D = 3; //dimensionality of data
 const size_t d = 2; //dimensionality of output
@@ -11,6 +12,83 @@ const double maxSigmaSq = 10e4;
 const double sigmaSqStart = (minSigmaSq+maxSigmaSq)/2;
 const double targetPerp = 2;
 const double eta = 0.9;
+const size_t maxIterations = 200;
+
+void calcHP(size_t i, size_t N, double* distances, double* betas, double* h, double *p)
+{
+	double sum = 0;
+	for (size_t j = 0; j < N; ++j)
+	{
+		p[i * N + j] = expf(- distances[i * N + j] * betas[i]);
+		sum += p[i * N + j];
+	}
+	double sumDP = 0;
+	for (size_t j = 0; j < N; ++j)
+	{
+		sumDP += p[i * N + j] * distances[i * N + j];
+	}
+	h[i] = log(sum) + sumDP/sum;
+}
+
+
+void binary_search_p_for_perplexity(double *X, size_t N, size_t D, double *p, double tol = 1e-5, double perplexity = 30.0)
+{
+	double sigmaMean = 0;
+	const double logU = logf(perplexity)/logf(2.0);
+	double *h = (double *) calloc(N, sizeof(double));
+	double *betas = (double*) malloc(N * sizeof(double));
+	memset(betas, 1.0, N); //TODO does this work?
+	double *distances = (double *) calloc(N*N, sizeof(double));
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			for (int k = 0; k < D; ++k)
+			{
+				//TODO: use symitry
+				const double d = X[i*k + k] - X[j*k + k];
+				distances[i * N + j] += d*d;
+			}
+		}
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		double betamin = -INFINITY	;
+		double betamax =  INFINITY;
+		//all distances but self iteractons
+		// Di = D[i, Math.concatenate((Math.r_[0:i], Math.r_[i+1:n]))];
+		calcHP(i, N, distances, betas, h, p);
+		double hdiff = h[i] - logU;
+		for (size_t tries = 0; (i < 50) && fabs(hdiff) > tol; ++tries)
+		{
+			if (hdiff > 0)
+			{
+				betamin = betas[i];
+				if (betamax == INFINITY || betamax == -INFINITY)
+					betas[i] = betas[i] * 2;
+				else
+					betas[i] = (betas[i] + betamax) / 2;
+			} else
+			{
+				betamax = betas[i];
+				if (betamin == INFINITY || betamin == -INFINITY)
+					betas[i] = betas[i] / 2;
+				else
+					betas[i] = (betas[i] + betamin) / 2;
+			}
+			
+			//Recompute the values
+			calcHP(i, N, distances, betas, h, p);
+			hdiff = h[i] - logU;
+			sigmaMean += sqrt(1/betas[i]);
+		}
+	}
+	sigmaMean /= N;
+	printf("Mean value of sigma: %lf", sigmaMean);
+}
+
+
 
 double calcPij(double* from, double* to, size_t n, double sigmaSq)
 {
@@ -91,142 +169,120 @@ int main(int argc, char const *argv[])
 	data[4*3 + 1] = 0.0;
 	data[4*3 + 2] = 25.0;
 
-
-	printf("calculating distances for pij\n");
-	double *pi_j = (double *) malloc(N*N*sizeof(double));
-	for(size_t i = 0; i < N; ++i)
+	double *p = (double *) calloc(N*N, sizeof(double));
+	double *iY = (double *) calloc(N*d, sizeof(double));
+	double *yd = (double *) malloc(N*d*sizeof(double));
+	double pSum = 0;
+	binary_search_p_for_perplexity(data, N, D, p);
+	// p = p + p.T
+	for (size_t i = 0; i < N; ++i)
+	for (size_t j = i; j < N; ++j)
 	{
-		for(size_t j = 0; j < i; ++j)
-		{
-			double nd = 0;
-			for(size_t k = 0; k < D; ++k)
-			{
-				double diff = data[i*D + k] - data[j*D + k];
-				nd += diff*diff;
-			}
-			pi_j[ i*N + j] = nd;
-			pi_j[ j*N + i] = nd;
-		}
-		pi_j[i*N + i] = 0;
+		const double pij = p[i * N + j] + p[j * N + i];
+		p[i * N + j] = pij;
+		if (i != j)
+			pSum += 2*pij;
+		else
+			pSum += pij;
 	}
 
-	printf("calculating pij\n");
-	double *tmp = (double *) malloc(N*sizeof(double));
-	for(size_t i = 0; i < N; ++i)
+	for (size_t i = 0; i < N; ++i)
+	for (size_t j = i; j < N; ++j)
 	{
-		printf("calculating for point i =%lu\n", i);
-		for(size_t j = 0; j < N; ++j) tmp[j] = pi_j[j];
-		double lower = minSigmaSq;
-		double upper = maxSigmaSq;
-		double current = sigmaSqStart;
-		double perp = calcPij(tmp, pi_j + (i*N) , i, current);
-		while ( fabs(perp - targetPerp) > 10e-1 )
-		{
-			printf("perp: %lf, targetPerp: %lf\n", perp, targetPerp);
-			if(perp > targetPerp)
-			{
-				upper = current;
-				current = (upper+lower)/2;
-			} else
-			{
-				lower = current;
-				current = (upper+lower)/2;
-			}
-			printf("calculating with %lf\n", current);
-			perp = calcPij(tmp, pi_j+(i*N), i, current);
-		}
+		//TODO check max function
+		const double pij = fmax(4*p[i * N + j]/pSum, 1e-12);
+		p[i * N + j] = pij;
+		p[j * N + i] = pij;
 	}
 
-	for(size_t i = 0; i < N; ++i)
+	//TODO initialize Y here
+
+	double *Y = (double *) calloc(N*d, sizeof(double));
+	double *one_over_y_dist_plus1 = (double *) calloc(N*N, sizeof(double));
+
+	for (size_t iteration = 0; iteration < maxIterations; ++iteration)
 	{
-		for(size_t j = 0; j < i; ++j)
+		//Compute pairwise affinities
+		double one_over_y_dist_plus1_sum = 0;
+		for (int i = 0; i < N; ++i)
 		{
-			double p = (pi_j[i*N + j] + pi_j[j*N + i])/(2*N);
-			pi_j[i*N + j] = p;
-			pi_j[j*N + i] = p;
-		}
-		pi_j[i*N + i] /= N; 
-	}
-
-	printf("initializing Y\n");
-	double *Yt = (double *) malloc(d*N*sizeof(double));
-	double *Yt1 = (double *) malloc(d*N*sizeof(double));
-	double *Yt2 = (double *) malloc(d*N*sizeof(double));
-	for(size_t i = 0; i < d*N; ++i)
-		Yt[i] = randn(0, 10e-4);
-
-
-	double *qij = (double *) malloc(N*N*sizeof(double));
-	double *y_diff = (double *) malloc(N*N*sizeof(double));
-	for(size_t t = 0; t < T; ++t)
-	{
-		printf("stepping t=%lu\n", t);
-		const double alpha = 0.1; //TODO adapt
-		double denom = 0;
-		for(size_t i = 0; i < N; ++i)
-		{
-			for(size_t j = 0; j < i; ++j)
+			for (int j = 0; j < N; ++j)
 			{
-				double nd = 0;
-				for(size_t k = 0; k < D; ++k)
+				if (i != j)
 				{
-					double diff = Yt[i*D + k] - Yt[j*D + k];
-					nd += diff*diff;
-				}
-				nd = 1.0/(1+nd);				
-				denom += nd;
-				qij[ i*N + j] = nd;
-				qij[ j*N + i] = nd;
-				y_diff[i*N + j] = nd;
-				y_diff[j*N + i] = nd;
-			}
-			qij[i*N + i] = 0;
-		}
-		for(size_t i = 0; i < N; ++i)
-			for(size_t j = 0; j < i; ++j)
-			{
-				qij[ i*N + j] /= denom;
-				qij[ j*N + i] /= denom;
-			}
-		for(size_t i = 0; i < N; ++i)
-		{
-			double grad[d] = {0};
-			double tmp[d];
-			for(size_t j = 0; j < N; ++j)
-			{
-				const double fact = eta*4*(pi_j[i*N + j] - qij[i*N + j])*y_diff[i*N + j];
-				for(size_t k = 0; k < d; ++k)
-				{
-					tmp[k] = Yt[i*d + k] + fact*(Yt[i*d + k] - Yt[j*d + k]);
-					if ( t > 1)
-						tmp[k] += alpha*(Yt1[i*d + k] - Yt2[i*d + k]);
+					double dy = 1;
+					for (int k = 0; k < d; ++k)
+					{
+						const double yi =  Y[i*d + k];
+						const double yj =  Y[j*d + k];
+						const double dist_yij = yi-yj;
+						dy += dist_yij*dist_yij;
+					}
+					one_over_y_dist_plus1[i * N + j] = 1.0/dy;
+					one_over_y_dist_plus1_sum += 1.0/dy;
 				}
 			}
-			for(size_t k = 0; k < d; ++k)
+		}
+
+		double momentum = final_momentum;
+		if (iter < 20)
+			momentum = initial_momentum;
+
+		for (int i = 0; i < N; ++i)
+		{
+			for (int j = 0; j < N; ++j)
+			for (int k = 0; k < d; ++k)
+				yd[i * N + j * d + k] = 0; //TODO rename dY - this is the gradient
+
+			for (int j = 0; j < N; ++j)
 			{
-		
-				Yt2[i*d + k] = Yt1[i*d + k];
-				Yt1[i*d + k] = Yt[i*d + k];
-				Yt[i*d + k] = tmp[k];
+				//TODO max
+				const double qij = max(1e-12, one_over_y_dist_plus1[i * N + j]/one_over_y_dist_plus1_sum);
+				double pq = p[i * N + j] - qij;
+				for (int k = 0; k < d; ++k)
+				{
+					const double yi =  Y[i*d + k];
+					const double yj =  Y[j*d + k];
+					const double gradij = pq * (yi - yj) * one_over_y_dist_plus1[i * N + j];
+					yd[i * N + j * d + k] += gradij;
+				}
 			}
+		}
+
+		for (int i = 0; i < N; ++i)
+		{
+			for (int k = 0; k < d; ++k)
+			{
+				iY[i*d + k] = momentum * iY[i*d + k] - eta * gains
+				const double qij = fmax(1e-12, one_over_y_dist_plus1[i * N + j]/one_over_y_dist_plus1_sum);
+				double pq = p[i * N + j] - qij;
+				for (int k = 0; k < d; ++k)
+				{
+					const double yi =  Y[i*d + k];
+					const double yj =  Y[j*d + k];
+					const double gradij = pq * (yi - yj) * one_over_y_dist_plus1[i * N + j];
+					yd[i * N + j * d + k] += gradij;
+				}
+			}
+		}
+
+		//Stop lying about P-values
+		for (size_t i = 0; i < N; ++i)
+		for (size_t j = i; j < N; ++j)
+		{
+			p[i * N + j] /= 4;
+			p[j * N + i] /= 4;
 		}
 	}
 
-	for(size_t i = 0; i < N; ++i)
-	{
-		printf("[");
-		for (size_t k = 0; k < d; ++k)
-			printf("%lf,", Yt[i*d + k]);
-		printf("]^T\n");
-	}
-
-	free(tmp);
-	free(Yt);
-	free(Yt1);
-	free(Yt2);
-	free(data);
-	free(pi_j);
-	free(qij);
-	free(y_diff);
+	//TODO frees
+	// free(tmp);
+	// free(Yt);
+	// free(Yt1);
+	// free(Yt2);
+	// free(data);
+	// free(pi_j);
+	// free(qij);
+	// free(y_diff);
 	return 0;
 }
