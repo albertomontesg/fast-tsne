8bf94dffc86410fe670c863a7ddda4fcc48409c9
Andi <abloec@student.ethz.ch>
2017-05-26 18:04:04 +0200

    beta update vectorized

--- code/implementations/optimizations/compute_pairwise_affinity_perplexity/compute_pairwise_affinity_perplexity.h
+++ code/implementations/optimizations/compute_pairwise_affinity_perplexity/compute_pairwise_affinity_perplexity.h
@@ -29,29 +29,30 @@ inline void perplexity_blocking(float* X, int N, int D, float* P,
 		float tol = 1e-5;
 		float sum_P;
 
-		__m256 beta_vec_neg = _mm256_set1_ps(-1*beta);
-		__m256 beta_vec_pos = _mm256_set1_ps(beta);
+		__m256 beta_vec = _mm256_set1_ps(1.0); // beta (init)
+        __m256 beta_min_vec = _mm256_set1_ps(-MAX_VAL);
+        __m256 beta_max_vec = _mm256_set1_ps(MAX_VAL);
+        __m256 half = _mm256_set1_ps(0.5);
+        __m256 two = _mm256_set1_ps(2);
+        __m256 minus = _mm256_set1_ps(-1);
 
-//	    printf("blocking: \n");
 		int iter = 0;
 		while (found == 0 && iter < 200) { // iter < 200
-//            printf("iter %d \n", iter);
 			// Compute Gaussian kernel row
 			// Compute entropy of current row
 			float H = 0.0;
 			sum_P = MIN_VAL; // = 0
 
 			int m;
-//            printf("-beta: %.30f \n", beta_vec_neg[0]);
 			for (m = 0; m + 8 <= N; m+=8){
+                // DD[m:m+7]
 				__m256 DD_row = _mm256_load_ps(DD+nN+m);
-				__m256 DD_row_beta = _mm256_mul_ps(beta_vec_neg,DD_row);
-//                for(int i=0; i<8; i++){
-//                    printf("(%d,%d) DD: %.30f \n", n, m+i, DD_row[i]);
-//                    printf("(%d,%d) exp arg: %.30f \n", n,m+i,DD_row_beta[i]);
-//                }
-				__m256 DD_row_beta_exp = _mm256_exp_ps(DD_row_beta);
-			//	__m256 DD_row_beta_pos = _mm256_mul_ps(beta_vec_pos,DD_row);
+                // -beta
+                __m256 DD_row_beta = _mm256_mul_ps(beta_vec, minus);
+                // -beta * DD[m:m+7]
+                DD_row_beta = _mm256_mul_ps(DD_row_beta, DD_row);
+				// exp(-beta * DD[m:m+7]
+                __m256 DD_row_beta_exp = _mm256_exp_ps(DD_row_beta);
 				_mm256_store_ps(P+nN+m,DD_row_beta_exp); // TODO: avoid storing back
 
 				if((n >= m) || (n < m + 8)){ // diagonal case
@@ -61,22 +62,12 @@ inline void perplexity_blocking(float* X, int N, int D, float* P,
 				__m256 P_row = _mm256_load_ps(P+nN+m);
 
                 // First calculate this before summing up P
-                __m256 DD_row_beta_P = _mm256_mul_ps(beta_vec_pos,DD_row);
+                __m256 DD_row_beta_P = _mm256_mul_ps(beta_vec,DD_row);
 				DD_row_beta_P = _mm256_mul_ps(DD_row_beta_P,P_row);
 
                 // summing up P
 				__m256 s = _mm256_hadd_ps(P_row,P_row);
 				sum_P += s[0] + s[1] + s[4] + s[5];
-
-//                for(int i=0; i<8; i++){
-//                    printf("row[%d] P[%d]: %.30f \n", n, m+i, P_row[i]);
-//                }
-//                for(int i=0; i<8; i++){
-//                    printf("row[%d] DD[%d]: %.30f \n", n, m+i, DD_row[i]);
-//                }
-//                for(int i=0; i<8; i++){
-//                    printf("row[%d] H[%d]: %.30f \n", n, m+i, DD_row_beta_P[i]);
-//                }
 
 				__m256 s1 = _mm256_hadd_ps(DD_row_beta_P,DD_row_beta_P);
 				H += s1[0] + s1[1] + s1[4] + s1[5];
@@ -93,11 +84,7 @@ inline void perplexity_blocking(float* X, int N, int D, float* P,
 
             float t1 = log_c(sum_P);
             float t2 = (H / sum_P);
-//           printf("H before: %.30f \n", H);
-//           printf("sum_P: %.30f \n", sum_P);
-//           printf("H part 1: %.30f \t part 2: %.30f \n", (H / sum_P), log_c(sum_P));
 			H = t1 + t2;
-//            printf("H after: %.30f \n", H);
 			// Evaluate whether the entropy is within the tolerance level
 			float Hdiff = H - log_c(perplexity);
 			if(Hdiff < tol && -Hdiff < tol) {
@@ -105,28 +92,26 @@ inline void perplexity_blocking(float* X, int N, int D, float* P,
 			}
 			else {
 				if(Hdiff > 0) {
-					min_beta = beta;
-					if(max_beta == MAX_VAL || max_beta == -MAX_VAL)
-						beta *= 2.0;
-					else
-						beta = (beta + max_beta) / 2.0;
+                    beta_min_vec = beta_vec;
+					if(beta_max_vec[0] == MAX_VAL || beta_max_vec[0] == -MAX_VAL){
+                        beta_vec = _mm256_mul_ps(two, beta_vec);
+                    } else{
+                        beta_vec = _mm256_add_ps(beta_vec, beta_max_vec);
+                        beta_vec = _mm256_mul_ps(half, beta_vec);
+                    }
+
 				}
 				else {
-					max_beta = beta;
-					if(min_beta == -MAX_VAL || min_beta == MAX_VAL)
-						beta /= 2.0;
-					else
-						beta = (beta + min_beta) / 2.0;
+                    beta_max_vec = beta_vec;
+					if(beta_min_vec[0] == -MAX_VAL || beta_min_vec[0] == MAX_VAL){
+                        beta_vec = _mm256_mul_ps(half, beta_vec);
+                    } else{
+                        beta_vec = _mm256_add_ps(beta_vec, beta_min_vec);
+                        beta_vec = _mm256_mul_ps(half, beta_vec);
+                    }
 				}
-                beta_vec_neg = _mm256_set1_ps(-1*beta);
-                beta_vec_pos = _mm256_set1_ps(beta);
 			}
 
-//		    printf("H: %.30f \n", H);
-//	        for(int i=0; i<N; i++){
-//	            printf("P[%d][%d]= %.30f \n", n, i, P[n*N+i]);
-//	        }
-//            printf("=======\n");
 			// Update iteration counter
 			iter++;
 		}
